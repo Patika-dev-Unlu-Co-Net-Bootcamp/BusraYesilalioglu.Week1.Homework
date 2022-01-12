258116b0c452046f1ff6a5d475f9037cc1ba2ac4
Büşra Yeşilalioğlu <88002831+7Auri@users.noreply.github.com>
2022-01-12 14:41:31 +0300

    Add files via upload

---
+++ UnluCo.Week1.HomeWork/Controllers/CatController.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnluCo.Week1.HomeWork.Controllers
+{
+    [Route("[controller]s")]
+    [ApiController]
+    public class CatController : ControllerBase
+    {
+
+        private static List<Cats> CatsList = new List<Cats>(){
+
+
+        new Cats
+        {
+            Id = 1,
+            Ad="Arya",
+            Irk="British Shorthair",
+            Cinsiyet="Dişi",
+            Yas= 2,
+            Renk="Siyah",
+            DogumTarihi= new DateTime(2020,11,27)
+
+
+        },
+        new Cats
+        {
+            Id = 2,
+            Ad="Oscar",
+            Irk="British Shorthair",
+            Cinsiyet="Erkek",
+            Yas= 0.6,
+            Renk="Gri-Beyaz-Siyah",
+            DogumTarihi= new DateTime(2021,06,29)
+
+            },
+        new Cats
+        {
+            Id = 3,
+            Ad="Pamir",
+            Irk="British Shorthair",
+            Cinsiyet="Erkek",
+            Yas= 0.6,
+            Renk="Gri",
+            DogumTarihi= new DateTime(2021,06,29)
+
+            },
+         new Cats
+        {
+            Id = 4,
+            Ad="Tarçın",
+            Irk="British Shorthair",
+            Cinsiyet="Erkek",
+            Yas= 0.6,
+            Renk="Kahverengi-Beyaz",
+            DogumTarihi= new DateTime(2021,06,29)
+
+            },
+          new Cats
+        {
+            Id = 5,
+            Ad="Mia",
+            Irk="British Shorthair",
+            Cinsiyet="Dişi",
+            Yas= 0.6,
+            Renk="Beyaz-Siyah",
+            DogumTarihi= new DateTime(2021,06,29)
+
+            }
+        };
+        [HttpGet("all")]
+       
+
+        public List<Cats> GetCats()
+        {
+            var catlist = CatsList.OrderBy(x => x.Id).ToList<Cats>();
+            return catlist;
+        }
+
+        [HttpGet("{id}/catswithId")]
+
+        public Cats GetById(int id)
+        {
+            var cat = CatsList.Where(cat => cat.Id == id).SingleOrDefault();
+            return cat;
+        }
+
+        //  [HttpGet]  // [HttpGet("{id}")]'in farklı bir kullanım şekli. Ama yukarıda da [HttpGet] olduğu için diğer [HttpGet] ile aynı anda çalıştırılamıyor.
+
+        //  public Cats Get([FromQuery]string id)
+        // {
+        //     var cat = CatsList.Where(cat => cat.Id == Convert.ToInt32(id)).SingleOrDefault();
+        //     return cat;
+        // }
+
+        [HttpPost("addNewCat")]
+
+        public IActionResult AddCat([FromForm] Cats newCat)
+        {
+            var cat = CatsList.SingleOrDefault(x => x.Id == newCat.Id);
+            if (cat != null)
+            {
+                return BadRequest();  //Send empty value açık olursa bu hata alınıyor.
+
+            }
+            CatsList.Add(newCat);
+            return StatusCode(201);
+        }
+
+        [HttpPut("{id}/updateCatInformations")]
+
+        public IActionResult UpdateCat(int id, [FromForm] Cats updateCat)
+        {
+
+            var cat = CatsList.SingleOrDefault(x => x.Id == id);
+            if (cat == null)
+            {
+                return BadRequest();
+            }
+            cat.Ad = updateCat.Ad != default ? updateCat.Ad : cat.Ad; // Eğer cat.Ad, updateCat.Ad'ten farklı ise cat.Ad'ı updateCat.Ad ile değiştir.
+            cat.Yas = updateCat.Yas != default ? updateCat.Yas : cat.Yas;
+            cat.Cinsiyet = updateCat.Cinsiyet != default ? updateCat.Cinsiyet : cat.Cinsiyet;
+            cat.Irk = updateCat.Irk != default ? updateCat.Irk : cat.Irk;
+            cat.Renk = updateCat.Renk != default ? updateCat.Renk : cat.Renk;
+            cat.DogumTarihi = updateCat.DogumTarihi != default ? updateCat.DogumTarihi : cat.DogumTarihi;
+            return Ok();
+
+        }
+        [HttpPatch("{id}/updateCatName")]
+        public IActionResult UpdateCatAd(int id, [FromBody] Cats updateCatAd)
+        {
+            var cat = CatsList.SingleOrDefault(x => x.Id == id);
+            if (cat == null)
+            {
+                return BadRequest();
+            }
+            cat.Ad = updateCatAd.Ad != default ? updateCatAd.Ad : cat.Ad;
+            return Ok();
+        }
+
+
+        [HttpDelete("{id}deleteCat")]
+
+        public IActionResult DeleteCat(int id)
+        {
+            var cat = CatsList.SingleOrDefault(x => x.Id == id);
+            if(cat == null)
+            {
+                return BadRequest();
+            }
+            CatsList.Remove(cat);
+            return Ok();
+
+        }
+
+
+    }
+}
---
+++ UnluCo.Week1.HomeWork/Models/Cat.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace UnluCo.Week1.HomeWork
+{
+    public class Cats
+    {
+       public int Id { get; set; }
+       public string Ad { get; set; }
+       public string Irk { get; set; }
+       public string Cinsiyet { get; set; }
+       public double Yas { get; set; }
+       public string Renk { get; set; }
+       public DateTime DogumTarihi { get; set; }
+    }
+}
